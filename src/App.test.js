3b41b18166a62ad9126e91e9e73ae683d6a4c5cd
Yuka <yuka1110m@gmail.com>
2017-12-05 18:27:04 +0100

    built a basic feature

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,23 @@
-/* global it, expect */
+/* global it, expect, jest */
 import React from 'react';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import App from './App';
+import { App } from './App';
+import { initialState } from './reducers';
 
 configure({ adapter: new Adapter() });
 
-
 it('App renders without crashing', () => {
-  const component = shallow(<App />);
+  const mockFunction = jest.fn();
+
+  const component = shallow(
+    <App
+      state={initialState}
+      submitTodo={mockFunction}
+      todos={[]}
+      deleteTodo={mockFunction}
+    />,
+  );
+  
   expect(component.exists()).toEqual(true);
 });
---
+++ src/components/todoList/test.js
@@ -0,0 +1,37 @@
+/* global describe, it, expect */
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import TodoList from '.';
+
+configure({ adapter: new Adapter() });
+
+describe('TodoList component', () => {
+  const deleteMock = jest.fn();
+
+  const props = {
+    todos: [
+      {
+        id: 1,
+        text: 'A todo',
+      },
+    ],
+    deleteTodo: deleteMock,
+  };
+
+  const component = shallow(<TodoList {...props} />);
+
+  it('Should render successfully', () => {
+    expect(component.exists()).toEqual(true);
+  });
+
+  it('Should display a todo when passed in as a prop', () => {
+    expect(component.find('.todo-text').text()).toEqual(props.todos[0].text);
+  });
+
+  it('Should call the deleteTodo function when Delete button is clicked', () => {
+    expect(deleteMock.mock.calls.length).toEqual(0);
+    component.find('.todo-delete').simulate('click');
+    expect(deleteMock.mock.calls.length).toEqual(1);
+  });
+});
